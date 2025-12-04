d4164a29bacb4a1e8fd7f5cc0465ce0e964e77da
Sowndaryamohan07 <127377354+Sowndaryamohan07@users.noreply.github.com>
2025-12-04 14:02:27 +0530

    Update LoginForm.tsx
    jenkins testing

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -194,7 +194,7 @@ export const LoginForm: React.FC = () => {
               <h1 className="text-2xl font-semibold text-gray-900 mb-1">RealXstate</h1>
               {/* <p className="text-sm text-gray-500 mb-6">Merchant Portal</p> */}
               <h2 className="text-xl font-medium text-gray-900">
-                Log in to your account
+                Login to your account
               </h2>
             </div>
 
